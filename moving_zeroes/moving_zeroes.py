9021e0c61d65150598139e9b1aff2a5efffc5819
yurika sakae <yurika.sun@gmail.com>
2020-06-10 23:30:08 -0700

    added bitwise XOR method to single number

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,18 +4,32 @@ Returns: a List of integers
 '''
 
 
+# def moving_zeroes(arr):
+#     count = 0
+
+#     for i in range(len(arr)):
+#         swap = False
+#         for j in range(len(arr)-i-1):
+#             if arr[j] == 0:
+#                 swap = True
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+#         if swap == False:
+#             count += 1
+#     return arr
+
 def moving_zeroes(arr):
     count = 0
-
-    for i in range(len(arr)):
-        swap = False
-        for j in range(len(arr)-i-1):
-            if arr[j] == 0:
-                swap = True
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-
-        if swap == False:
+    l = len(arr)
+    for i in range(l):
+        if arr[i] != 0:
+            arr[count] = arr[i]
             count += 1
+
+    while count < l:
+        arr[count] = 0
+        count += 1
+    print(arr)
     return arr
 
 
