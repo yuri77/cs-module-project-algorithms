55e70d4c9c5a3db0514b7f091857482de27a5a6e
yurika sakae <yurika.sun@gmail.com>
2020-06-10 23:12:06 -0700

    brute-force solution moving_zeros complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,21 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    count = 0
+
+    for i in range(len(arr)):
+        swap = False
+        for j in range(len(arr)-i-1):
+            if arr[j] == 0:
+                swap = True
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+        if swap == False:
+            count += 1
+    return arr
 
 
 if __name__ == '__main__':
